8e965d85f20e3e4f6558b485dd5d49391f53df2f
Daniil Shevtsov <daniil.shevtsov@aviasales.ru>
2024-11-24 15:32:30 +0400

    mouse movement works

--- Game.cs
+++ Game.cs
@@ -5,11 +5,13 @@ using System.Collections.Generic;
 public partial class Game : Node2D, Unit
 {
 
+	private Vector2 targetPosition = Vector2.Zero;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		player = GetNode<Player>("Player");
+		targetPosition = player.GlobalPosition;
 		enemy = GetNode<Enemy>("Enemy");
 		camera = GetNode<Camera2D>("Camera2D");
 
@@ -18,22 +20,44 @@ public partial class Game : Node2D, Unit
 
 	public override void _PhysicsProcess(double delta)
 	{
-		Vector2 velocity = player.Velocity;
-		Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
-		if (direction != Vector2.Zero)
+		if (targetPosition != player.GlobalPosition)
 		{
-			velocity = direction * Player.Speed;
-		}
-		else
-		{
-			velocity = new Vector2(
-				Mathf.MoveToward(player.Velocity.X, 0, Player.Speed),
-				Mathf.MoveToward(player.Velocity.Y, 0, Player.Speed)
-			);
-		}
+			Vector2 velocity = player.Velocity;
+			Vector2 direction = (targetPosition - player.GlobalPosition).Normalized();
+			if (direction != Vector2.Zero)
+			{
+				velocity = direction * Player.Speed;
+			}
+			else
+			{
+				velocity = new Vector2(
+					Mathf.MoveToward(player.Velocity.X, 0, Player.Speed),
+					Mathf.MoveToward(player.Velocity.Y, 0, Player.Speed)
+				);
+			}
 
-		player.Velocity = velocity;
-		player.MoveAndSlide();
+			player.Velocity = velocity;
+			player.MoveAndSlide();
+		}
+	}
+
+	public override void _UnhandledInput(InputEvent @event)
+	{
+		base._UnhandledInput(@event);
+		if (@event is InputEventMouseButton eventMouseButton && eventMouseButton.IsReleased())
+		{
+			var velocity = Vector2.Zero;
+			var mousePosition = GetGlobalMousePosition();
+			targetPosition = mousePosition;
+			// velocity = mousePosition - player.GlobalPosition;
+			// velocity = new Vector2(
+			// 		Mathf.MoveToward(player.Velocity.X, velocity.X, Player.Speed),
+			// 		Mathf.MoveToward(player.Velocity.Y, velocity.Y, Player.Speed)
+			// );
+			// targetVelocity = velocity;
+			//player.Velocity = velocity;
+			//player.MoveAndSlide();
+		}
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
