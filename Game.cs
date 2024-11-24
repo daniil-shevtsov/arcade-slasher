6ff7c0010cd9c801b9a9bcf494387953977df5e2
Daniil Shevtsov <daniil.shevtsov@aviasales.ru>
2024-11-24 15:20:48 +0400

    add keyboard movement

--- Game.cs
+++ Game.cs
@@ -16,6 +16,26 @@ public partial class Game : Node2D, Unit
 		units = new List<Unit>() { player, enemy };
 	}
 
+	public override void _PhysicsProcess(double delta)
+	{
+		Vector2 velocity = player.Velocity;
+		Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
+		if (direction != Vector2.Zero)
+		{
+			velocity = direction * Player.Speed;
+		}
+		else
+		{
+			velocity = new Vector2(
+				Mathf.MoveToward(player.Velocity.X, 0, Player.Speed),
+				Mathf.MoveToward(player.Velocity.Y, 0, Player.Speed)
+			);
+		}
+
+		player.Velocity = velocity;
+		player.MoveAndSlide();
+	}
+
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
